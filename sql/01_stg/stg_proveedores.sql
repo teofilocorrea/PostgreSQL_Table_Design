62b5994f8de5182bfb40a0505c00748294f30a0e
Teófilo Correa Rojas <teofilocorrea@gmail.com>
2026-05-09 14:14:25 -0400

    fix(stg): recreate stg.proveedores table removing UNIQUE constraint on email field

--- sql/01_stg/stg_proveedores.sql
+++ sql/01_stg/stg_proveedores.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS stg.proveedores (
     id          SERIAL          PRIMARY KEY,
     nombre      VARCHAR(100)    NOT NULL,
     contacto    VARCHAR(150),
-    email       VARCHAR(150)    UNIQUE,
+    email       VARCHAR(150),
     telefono    VARCHAR(15),
     pais        VARCHAR(100),
     activo      BOOLEAN         NOT NULL,
